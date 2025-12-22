f6f8c11f99e37e68309d75719540eea0ab612e8c
jruiz <42394308+jarr59@users.noreply.github.com>
2025-12-21 21:12:04 -0600

    3-Agregar modelo

---
+++ src/FinanceTracker.Domain/Transaction/Transaction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinanceTracker.Domain.Transaction
+{
+    public class Transaction
+    {
+        public Guid Id { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public TransactionType Type { get; set; }
+    }
+}
