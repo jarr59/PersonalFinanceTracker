b0e8e604edb36079545d2201cc01ca533fe0b343
jruiz <42394308+jarr59@users.noreply.github.com>
2026-01-20 22:03:47 -0600

    8-Agregar el notificacion state

---
+++ src/FinanceTracker.Web/States/Errors/ErrorState.cs
@@ -0,0 +1,25 @@
+﻿namespace FinanceTracker.Web.States.Errors;
+
+public sealed class ErrorState : StateContainerBase
+{
+    public List<string> Errors { get; private set; } = [];
+
+    /// <summary>
+    /// Agrega un error al listado de errores
+    /// </summary>
+    /// <param name="message"></param>
+    public void Add(string message)
+    {
+        Errors.Add(message);
+        NotifyStateChanged();
+    }
+
+    /// <summary>
+    /// Limpia el listado de errores
+    /// </summary>
+    public void Clear()
+    { 
+        Errors.Clear(); 
+        NotifyStateChanged();
+    }
+}
